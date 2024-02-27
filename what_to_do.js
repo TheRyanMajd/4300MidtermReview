b9675f6cdb21ed6503cf37abbf68065b449989ac
TheRyanMajd <rm97798@uga.edu>
2024-02-27 17:04:42 -0500

    first init

---
+++ what_to_do.js
@@ -0,0 +1,56 @@
+// JavaScript
+
+// Run-time environment - browser/node
+
+// Callbacks / Higher order functions
+
+// Anonymous, arrow functions 
+
+// variables & scope - const, let, var 
+
+// javascript functions - objects
+
+// javascript types
+
+// Document Object Module - DOM 
+
+// DOM manipulation to add/change elements
+
+// Nodes and node relationships of the DOM
+
+// Root node - document object
+
+// Selecting nodes - querySelector, querySelectorAll, by tag, id, class
+
+// Steps to add a new node
+
+// 3 ways to write  asynchronous code in JavaScript 
+
+// callbacks, 
+// promises - 3 states of a promise, how to chain, possible outcomes
+// async/await
+// Trace execution of asynchronous code (callbacks, promises, async/await)
+
+// Asynchronous vs synchronous 
+
+// Why asynchronous on web
+
+// Event handling - form of asynchronous programming
+
+// Event Loop, call stack, background tasks, task queue
+
+// Arrays - size, types, can start empty and grow
+
+// Array methods - forEach, map, filter, sort, reduce
+
+// **write
+
+// Arrow function
+
+// Higher order function that accepts a callback
+
+// DOM event listener
+
+// JavaScript array methods 
+
+// JavaScript to handle asynchronous call that returns a promise (either promise or async/await)
